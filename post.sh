4371f168a7088971f461921791b2bf9599a4855f
Misko <misko@meta.com>
2024-05-06 23:22:06 +0000

    updates

--- post.sh
+++ post.sh
@@ -1,19 +1,20 @@
 find . -type f -name '*.py' | xargs sed -i 's/ocpmodels_root/fairchem_core_root/g'
 find . -type f -name '*.py' | xargs sed -i 's/ocpmodels/fairchem.core/g'
 find . -type f -name '*.py' | xargs sed -i 's/from ocpapi/from fairchem.demo.ocpapi/g'
-find . -type f -name '*.py' | xargs sed -i 's/from ocdata/from fairchem.datagen.oc/g'
+find . -type f -name '*.py' | xargs sed -i 's/from ocdata/from fairchem.data.oc/g'
 find . -type f -name '*.md' | xargs sed -i 's/from ocpmodels/from fairchem.core/g'
 find . -type f -name '*.md' | xargs sed -i 's/from ocpapi/from fairchem.demo.ocpapi/g'
-find . -type f -name '*.md' | xargs sed -i 's/from ocdata/from fairchem.datagen.oc/g'
+find . -type f -name '*.md' | xargs sed -i 's/from ocdata/from fairchem.data.oc/g'
 sed -i 's@"fairchem.core"@"core"@g' src/fairchem/core/common/utils.py 
 sed -i  's@^data$@/data\n/src/fairchem/data@g' .gitignore
 git add -u 
 git commit -m 'rename imports for monorepo'
 git mv src/fairchem/demo/ocpapi/ocpapi/* ./src/fairchem/demo/ocpapi/
-git mv src/fairchem/datagen/oc/ocdata/* ./src/fairchem/datagen/oc/
+git mv src/fairchem/data/oc/ocdata/* ./src/fairchem/data/oc/
 git add -u 
 git commit -m 'folder promote ocpapi and open-catalyst-dataset'
-cp -rf ../extras/.github ../extras/packages ../extras/src ../extras/README.md ../extras/main.py ./
+cp -rf ../extras/.github ../extras/packages ../extras/src ../extras/README.md ../extras/main.py ../extras/LICENSE.md ./
+git add main.py README.md LICENSE.md
 git add -u
 git commit -m 'fixes to workflow'
 cp -f ../extras/src/fairchem/core/common/utils.py ./src/fairchem/core/common/
@@ -22,7 +23,7 @@ git commit -m 'fix yaml load'
 git add packages 
 git commit -m 'add packages'
 ruff check --fix --statistics --config packages/fairchem-core/pyproject.toml src/fairchem/core/
-ruff check --fix --statistics --config packages/fairchem-datagen-oc/pyproject.toml src/fairchem/datagen/oc/
+ruff check --fix --statistics --config packages/fairchem-data-oc/pyproject.toml src/fairchem/data/oc/
 git add -u
 git commit -m 'ruff fixes'
 find src/ | grep .gitignore | xargs git rm 
